5166a455bbc75d32215633c730e21788445c721a
Justin Rowles <justin.rowles@bgch.co.uk>
2016-03-30 11:04:52 +0100

    Fail a query if any connection to consul as part of that query fails

--- lib/hiera/backend/consul_backend.rb
+++ lib/hiera/backend/consul_backend.rb
@@ -14,7 +14,7 @@ class Hiera
       end
 
       def lazy_connection_setup(scope) 
-        if @connection.nil?
+        if @consul.nil?
           @host =  Backend.parse_string(@config[:host], scope) 
           Hiera.debug("[hiera-consul]: Using host #{@host}")
           @consul = Net::HTTP.new(@host, @config[:port])
@@ -47,11 +47,6 @@ class Hiera
       end
 
       def lookup(key, scope, order_override, resolution_type)
-        lazy_connection_setup(scope) 
-
-        host =  Backend.parse_string(@config[:host], scope, { 'key' => key }) 
-        Hiera.debug("[hiera-consul]: Using connection #{@consul} to host #{host}")
-
         if resolution_type == :array
           answer = []
         elsif resolution_type == :hash
@@ -60,62 +55,43 @@ class Hiera
           answer = nil
         end
 
-        paths = @config[:paths].map { |p| Backend.parse_string(p, scope, { 'key' => key }) }
-        paths.insert(0, order_override) if order_override
+        begin
+          lazy_connection_setup(scope) 
+        # Any failure to talk to consul results in the entire request failing.
+        rescue Exception => e
+          @consul=nil
+          Hiera.debug("[hiera-consul]: Could not connect to Consul: #{e}")
+          raise Exception, e.message unless @config[:failure] == 'graceful'
+          return answer
+        end
 
-        catch (:found) do
-          paths.each do |path|
-            Hiera.debug("[hiera-consul]: Checking path #{path}")
-            # We only support querying the catalog or the kv store
-            if path !~ /^\/v\d\/(catalog|kv)\//
-              Hiera.debug("[hiera-consul]: We only support queries to catalog and kv and you asked #{path}, skipping")
-              next
-            end
-            # Check that we are not looking somewhere that will make hiera crash subsequent lookups
-            if "#{path}".match("//")
-              Hiera.debug("[hiera-consul]: The specified path #{path}/#{key_delimited} is malformed, skipping")
-              next
-            end
-            if path == 'services'
-              if @cache.has_key?(key)
-                answer = @cache[key]
-                return answer
+        begin
+          paths = @config[:paths].map { |p| Backend.parse_string(p, scope, { 'key' => key }) }
+          paths.insert(0, order_override) if order_override
+
+          catch (:found) do
+            paths.each do |path|
+              Hiera.debug("[hiera-consul]: Checking path #{path}")
+              # We only support querying the catalog or the kv store
+              if path !~ /^\/v\d\/(catalog|kv)\//
+                Hiera.debug("[hiera-consul]: We only support queries to catalog and kv and you asked #{path}, skipping")
+                next
               end
-            end
-  
-
-  #lookup fully qualified path
-            [key, key.gsub('::', '/')].each do | key_delimited | 
-              this_answer = wrapquery("#{path}/#{key_delimited}")
-              if resolution_type == :array
-                answer = answer + this_answer unless ! this_answer
-              elsif resolution_type == :hash
-                answer = this_answer.merge(answer) unless ! this_answer #Earliest value takes precedence
-              else #if resolution_type == :priority
-                answer = this_answer 
-                throw :found if answer
+              # Check that we are not looking somewhere that will make hiera crash subsequent lookups
+              if "#{path}".match("//")
+                Hiera.debug("[hiera-consul]: The specified path #{path}/#{key_delimited} is malformed, skipping")
+                next
               end
-            end
-
-  #lookup partial path plus yaml/json
-            [key, key.gsub('::', '/')].each do | key_delimited | 
-              key_parts=key_delimited.split("/")
-              Hiera.debug("[hiera-consul]: key_delimited is now #{key_delimited}")
-              Hiera.debug("[hiera-consul]: key_parts is now #{key_parts}")
-              # search most specific first
-              hash_key_parts=[]
-              while key_parts.size>0 do
-                hash_key_parts = hash_key_parts + [key_parts.pop]
-                key_reconstructed=key_parts.join('/')
-                this_answer = wrapquery("#{path}/#{key_reconstructed}")
-                hash_key_parts_copy = hash_key_parts
-                while hash_key_parts_copy.size>0 do
-                  break unless this_answer.is_a? Hash 
-                  this_answer=this_answer[hash_key_parts_copy[0]]
-                  hash_key_parts_copy.shift
+              if path == 'services'
+                if @cache.has_key?(key)
+                  answer = @cache[key]
+                  return answer
                 end
-                next if this_answer.nil?
-                next if hash_key_parts_copy.size>0
+              end
+    
+              #lookup fully qualified path
+              [key, key.gsub('::', '/')].each do | key_delimited | 
+                this_answer = wrapquery("#{path}/#{key_delimited}")
                 if resolution_type == :array
                   answer = answer + this_answer unless ! this_answer
                 elsif resolution_type == :hash
@@ -125,34 +101,68 @@ class Hiera
                   throw :found if answer
                 end
               end
-            end
 
-  #lookup base path plus yaml/json
-            [key, key.gsub('::', '/')].each do | key_delimited | 
-              hash_key_parts=key_delimited.split("/")
-              this_answer = wrapquery("#{path}")
-              while hash_key_parts.size>0 do
-                break unless this_answer.is_a? Hash 
-                this_answer=this_answer[hash_key_parts[0]]
-                hash_key_parts.shift
+              #lookup partial path plus yaml/json
+              [key, key.gsub('::', '/')].each do | key_delimited | 
+                key_parts=key_delimited.split("/")
+                Hiera.debug("[hiera-consul]: key_delimited is now #{key_delimited}")
+                Hiera.debug("[hiera-consul]: key_parts is now #{key_parts}")
+                # search most specific first
+                hash_key_parts=[]
+                while key_parts.size>0 do
+                  hash_key_parts = hash_key_parts + [key_parts.pop]
+                  key_reconstructed=key_parts.join('/')
+                  this_answer = wrapquery("#{path}/#{key_reconstructed}")
+                  hash_key_parts_copy = hash_key_parts
+                  while hash_key_parts_copy.size>0 do
+                    break unless this_answer.is_a? Hash 
+                    this_answer=this_answer[hash_key_parts_copy[0]]
+                    hash_key_parts_copy.shift
+                  end
+                  next if this_answer.nil?
+                  next if hash_key_parts_copy.size>0
+                  if resolution_type == :array
+                    answer = answer + this_answer unless ! this_answer
+                  elsif resolution_type == :hash
+                    answer = this_answer.merge(answer) unless ! this_answer #Earliest value takes precedence
+                  else #if resolution_type == :priority
+                    answer = this_answer 
+                    throw :found if answer
+                  end
+                end
               end
-              next if this_answer.nil?
-              next if hash_key_parts.size>0
-              if resolution_type == :array
-                answer = answer + this_answer unless ! this_answer
-              elsif resolution_type == :hash
-                answer = this_answer.merge(answer) unless ! this_answer #Earliest value takes precedence
-              else #if resolution_type == :priority
-                answer = this_answer 
-                throw :found if answer
-              end
-            end
 
-            Hiera.debug("[hiera-consul]: Answer is now #{answer}")
+              #lookup base path plus yaml/json
+              [key, key.gsub('::', '/')].each do | key_delimited | 
+                hash_key_parts=key_delimited.split("/")
+                this_answer = wrapquery("#{path}")
+                while hash_key_parts.size>0 do
+                  break unless this_answer.is_a? Hash 
+                  this_answer=this_answer[hash_key_parts[0]]
+                  hash_key_parts.shift
+                end
+                next if this_answer.nil?
+                next if hash_key_parts.size>0
+                if resolution_type == :array
+                  answer = answer + this_answer unless ! this_answer
+                elsif resolution_type == :hash
+                  answer = this_answer.merge(answer) unless ! this_answer #Earliest value takes precedence
+                else #if resolution_type == :priority
+                  answer = this_answer 
+                  throw :found if answer
+                end
+              end
+
+              Hiera.debug("[hiera-consul]: Answer is now #{answer}")
+            end
           end
+        # Any failure to talk to consul results in the entire request failing.
+        rescue Exception => e
+          Hiera.debug("[hiera-consul]: Could not connect to Consul: #{e}")
+          raise Exception, e.message unless @config[:failure] == 'graceful'
         end
-        answer
-      end
+      answer
+    end
 
       def parse_result(res)
           require 'base64'
@@ -197,13 +207,9 @@ class Hiera
 
           httpreq = Net::HTTP::Get.new("#{path}#{token(path)}")
           answer = nil
-          begin
-            result = @consul.request(httpreq)
-          rescue Exception => e
-            Hiera.debug("[hiera-consul]: Could not connect to Consul")
-            raise Exception, e.message unless @config[:failure] == 'graceful'
-            return answer
-          end
+
+          # Exceptions will be caught at lookup level.
+          result = @consul.request(httpreq)
           unless result.kind_of?(Net::HTTPSuccess)
             Hiera.debug("[hiera-consul]: HTTP response code was #{result.code}")
             return answer
